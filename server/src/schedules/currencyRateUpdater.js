9ac79e982bc711b4536263f5383c881a5a785476
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2017-10-27 21:17:49 +0300

    Call update currencies every night at 2AM

--- server/src/schedules/currencyRateUpdater.js
+++ server/src/schedules/currencyRateUpdater.js
@@ -1,3 +1,7 @@
+import { getCurrencies } from '../routes/currencies';
+
 export default function currencyRateUpdater() {
-  console.log('Hello World');
+  // Not sure if this is a great idea... but basically we call the handler function
+  // for the "get /currencies" request, with an empty context.
+  getCurrencies({});
 }
--- server/src/schedules/index.js
+++ server/src/schedules/index.js
@@ -1,8 +1,9 @@
 import schedule from 'node-schedule';
 import currencyRateUpdater from './currencyRateUpdater';
 
-const EVERY_MINUTE = '* * * * *';
+// const EVERY_MINUTE = '* * * * *';
+const AT_2_AM = '0 2 * * *';
 
 export default function setupSchedulers() {
-  schedule.scheduleJob(EVERY_MINUTE, currencyRateUpdater);
+  schedule.scheduleJob(AT_2_AM, currencyRateUpdater);
 }
