d71816cf6e61eb06e3e64ee84ca3f6c53c4e2b9c
Joachim Seminck <joachim.seminck@pipedrive.com>
2018-01-14 21:48:11 +0200

    TT2 commands

--- server/src/middlewares/index.js
+++ server/src/middlewares/index.js
@@ -1,11 +1,12 @@
-import koaBody from 'koa-body';
-const serve = require('koa-static');
 import setupCors from './cors';
 import setupErrorHandler from './errors';
 import setupLogger from './logger';
+import koaBody from 'koa-body';
+
+const serve = require('koa-static');
 
 export default function setupMiddlewares(app) {
-  app.use(koaBody{ multipart: true }());
+  app.use(koaBody({ multipart: true }));
   app.use(serve(path.join(__dirname, '/screenshots')));
 
   setupErrorHandler(app);
